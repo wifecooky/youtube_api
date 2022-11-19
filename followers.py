5756a6b484be830856144e1ac23e942c4c7a0e9f
wen <wifecooky@gmail.com>
2022-11-19 21:23:19 +0900

    init

---
+++ followers.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+import requests
+import json
+
+CHANNEL_ID = 'UCFo4kqllbcQ4nV83WCyraiw'
+YOUTUBE_API_KEY = 'AIzaSyAUSXeY5S3P7wkWq8Pir_YB3B2_jDURg9w'
+
+# Get youtube channel followers
+def get_youtube_channel_followers():
+    url = 'https://www.googleapis.com/youtube/v3/channels?part=statistics&id=' + CHANNEL_ID + '&key=' + YOUTUBE_API_KEY
+    response = requests.get(url)
+    data = json.loads(response.text)
+    followers = data['items'][0]['statistics']['subscriberCount']
+    return followers
+
+if __name__ == '__main__':
+    followers = get_youtube_channel_followers()
+    print(followers)
